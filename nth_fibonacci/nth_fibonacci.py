d5b8f17313d9ed1de7bcf7da99a86770c2849710
Ali Nategh <a.nategh76@gmail.com>
2022-07-26 12:59:10 +0430

    updated fibonacci

--- nth_fibonacci/nth_fibonacci.py
+++ nth_fibonacci/nth_fibonacci.py
@@ -1,3 +1,4 @@
+from math import sqrt
 
 # time: O(2^n) | space: O(n)
 def fib1(n):
@@ -16,16 +17,30 @@ def fib2(n, memoize = {1: 0, 2: 1}):
         memoize[n] = fib2(n-1, memoize) + fib2(n-2, memoize)
         return memoize[n]
 
+# time: O(n) | space: O(1)
 def fib3(n):
-    lastTwo = [0, 1]
-    counter = 2
-    while counter < n:
-        nextFib = lastTwo[0] + lastTwo[1]
-        lastTwo[0] = lastTwo[1]
-        lastTwo[1] = nextFib
-        counter += 1
-    return lastTwo[1]
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        lastTwo = [0, 1]
+        counter = 2
+        while counter < n:
+            nextFib = lastTwo[0] + lastTwo[1]
+            lastTwo[0] = lastTwo[1]
+            lastTwo[1] = nextFib
+            counter += 1
+        return lastTwo[1]
+
+def fib4(n):
+    # using fibonacci's proven constant time formula
+    n = n-1 # n starts from zero in this formula
+    res = (((1+sqrt(5))**n)-((1-sqrt(5)))**n)/(2**n*sqrt(5))
+    return int(res)
+
 
 print(fib1(9))
 print(fib2(9))
 print(fib3(9))
+print(fib4(9))
